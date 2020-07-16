7742e45848d748ddc5ad634f7daace0601a5ff02
Ricky Talbot <rickytalbot@hotmail.com>
2020-07-16 10:18:48 -0600

    Removed code still based on the node-resolver plugin and astw to instead use the built in tree walking and tokenization features, effectively removing all external dependencies. Updated README to reflect new features.

--- README.md
+++ README.md
@@ -1,40 +1,22 @@
-<!-- # node-resolver [![stable](http://badges.github.io/stability-badges/dist/stable.svg)](http://github.com/badges/stability-badges)
+# adonis-resolver
 
-Atom plugin to jump to a module's file from its require statement.
-
-![node-resolver](http://imgur.com/1JW3Txg.gif)
+Atom plugin to jump to either a module's file from its use or make statement in a Javascript file or an included template from it's @[!]component/@include/@extends/@layout statement in an Edge file.
 
 ## Usage
 
-1. Move your cursor to a require statement.
-2. Open the command palette and run the "Node Resolver: Open Selected Dependencies" command.
+1. Move your cursor to the textual portion of the statement to resolve.
+2. Open the command palette and run the "Adonis Resolver: Open Selected Dependencies" command.
 3. The selected file will open.
-
-Note that you can also select a range of text and run the command to open up
-*all* of the files required within that range.
 
 If you'd like to add a shortcut for this, I'd recommend adding the following
 to your `keymap.cson`:
 
 ``` cson
 '.workspace':
-  'ctrl-alt-o': 'node-resolver:open-selected-dependencies'
+  'ctrl-alt-o': 'adonis-resolver:open-selected-dependencies'
 ```
 
-## License
-
-MIT. See [LICENSE.md](http://github.com/hughsk/atom-node-resolver/blob/master/LICENSE.md) for details. -->
-# adonis-resolver
-
-Atom plugin to jump to a module's file from its use or make statement.
-
-## Usage
-
-1. Move your cursor to a use or make statement.
-2. Open the command palette and run the "Adonis Resolver: Open Selected Dependencies" command.
-3. The selected file will open.
-
-Alternatively, you can use Hyperclicking to do the above more quickly.
+Alternatively, you can use Hyperclicking on a statement to do the above more quickly.
 
 # License
 
--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-const position = require('file-position');
-const eval = require('static-eval');
-const esprima = require('esprima');
-const clone = require('clone');
 const path = require('path');
-const astw = require('astw');
 const fs = require('fs');
 const {Point, Range} = require('atom');
 
@@ -17,7 +12,7 @@ exports.provideAdonisHyperclick = function() {
         priority: 0,
         wordRegExp: /(use|make)\s*\(\s*["'].+?["']\s*\)|(@component|@!component|@extends|@layout|@include)\s*\(\s*["'].+?["']\s*[),]/g,
         getSuggestionForWord: function(editor, string, range) {
-            // Does not properly handle the case that a use call spans multiple lines, but wordRegExp seems to prevent that anyways.
+            // Does not properly handle the case that a match spans multiple lines, but wordRegExp seems to prevent that anyways.
             let match = string.match(/'(.+)'/);
             let start = range.start.column + match.index + 1;
             let end = start + match[1].length;
@@ -31,13 +26,11 @@ function handleOpenDependencies() {
     let editor = atom.workspace.getActiveTextEditor();
     let ranges = editor.getSelectedBufferRanges().slice();
     let dir = atom.project.getPaths().filter(p => editor.getPath().includes(p))[0];
-    let src = editor.getBuffer().getText();
 
     switch(editor.languageMode.grammar.scopeName) {
         case "source.js":
         case "source.ts": {
-            let ast = esprima.parse(src, {loc: true});
-            astw(ast)(handleAstNode(ranges, dir, position(src)));
+            handleJs(editor, ranges, dir);
             break;
         }
         case "text.html.edge": {
@@ -47,53 +40,36 @@ function handleOpenDependencies() {
     }
 }
 
-// This should be re-written to use the build in parsing trees instead of constructing our own
-function handleAstNode(ranges, dir, lookup) {
-    return node => {
-        if (!(nodeIs("use")(node) || nodeIs("make")(node))) return;
-        if (!node.arguments) return;
-        if (!node.arguments.length) return;
+function handleJs(editor, ranges, dir) {
+    for (let range of ranges) {
+        let tokenLine = editor.getBuffer().getLanguageMode().tokenizedLineForRow(range.start.row);
+        let token = tokenLine.tokenAtBufferColumn(range.start.column);
 
-        let dst = node.evalled = node.evalled || eval(node.arguments[0]);
-        if (!dst) return;
+        if (token.scopes.includes("string.quoted")) {
+            // First if it's a project file
+            let parts = token.value.slice(1, -1).split('/');
+            let name = parts[parts.length - 1];
+            let file = path.resolve(dir, parts[0].toLowerCase(), parts.slice(1, parts.length - 1).join('/'), name + ".js");
+            if (fs.existsSync(file)) return atom.workspace.open(file);
 
-        let included = false;
-
-        for (let i = 0; i < ranges.length; i++) {
-            let loc = clone(node.loc);
-
-            loc.start.line--;
-            loc.end.line--;
-
-            let a = getIndex(lookup, ranges[i]);
-            let b = getIndex(lookup, loc);
-
-            if (included = overlap(a, b)) break;
-        }
-
-        if (!included) return;
-
-        // First if it's a project file
-        let parts = dst.split('/');
-        let name = parts[parts.length - 1];
-        let file = path.resolve(dir, parts[0].toLowerCase(), parts.slice(1, parts.length - 1).join('/'), name + ".js");
-        if (fs.existsSync(file)) return atom.workspace.open(file);
-
-        // Then if it's an Adonis file
-        let adonisDir = path.resolve(dir, "node_modules", "@adonisjs");
-        for (let subDir of fs.readdirSync(adonisDir)) {
-            let srcDir = path.resolve(adonisDir, subDir, "src");
-            if (fs.existsSync(srcDir)) {
-                let ret = checkSubModules(srcDir, name);
-                if (ret) return ret;
+            // Then if it's an Adonis file
+            let adonisDir = path.resolve(dir, "node_modules", "@adonisjs");
+            for (let subDir of fs.readdirSync(adonisDir)) {
+                let srcDir = path.resolve(adonisDir, subDir, "src");
+                if (fs.existsSync(srcDir)) {
+                    let ret = checkSubModules(srcDir, name);
+                    if (ret) return ret;
+                }
             }
+
+            // Need an inexpensive way to check every other module for any possible class Adonis might know about,
+            // for now classes from other non-adonis modules and some odly named classes will not be found
+
+            return atom.notifications.addWarning("No source for module ''" + name + "'' found.");
         }
 
-        // Need an inexpensive way to check every other module for any possible class Adonis might know about,
-        // for now classes from other non-adonis modules and some odly named classes will not be found
-
-        return atom.notifications.addWarning("No source for module ''" + name + "'' found.");
-    };
+    }
+    return atom.notifications.addError("Cannot go to file at this location.");
 }
 
 function handleEdge(editor, ranges, dir) {
@@ -127,25 +103,4 @@ function checkSubModules(dir, name) {
             checkSubModules(modDir, name);
         }
     }
-}
-
-function overlap(a, b) {
-    return (a.start >= b.start && a.start <= b.end) || (b.start >= a.start && b.start <= a.end);
-}
-
-function getIndex(lookup, range, off) {
-    off = off || 0;
-    return {
-            start: lookup((range.start.row || range.start.line) + off, range.start.column),
-            end: lookup((range.end.row || range.end.line) + off, range.end.column)
-        };
-}
-
-function nodeIs(word) {
-    word = word || 'require';
-
-    return node => {
-            let c = node && node.callee;
-            return c && c.name === word && c.type === 'Identifier' && node.type === 'CallExpression'
-        };
 }
